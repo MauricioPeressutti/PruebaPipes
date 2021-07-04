2f2a8b271941fbee5ebd248930d2584a846a1173
MauricioPeressutti <82254015+MauricioPeressutti@users.noreply.github.com>
2021-07-04 00:34:16 -0300

    fix 1.0
    fix 1.0 prueba pipes

--- pipes/src/app/app.component.ts
+++ pipes/src/app/app.component.ts
@@ -2,9 +2,17 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  templateUrl: './app.component.html', // or whatever filename
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'pipes';
+ 
+
+  nombre:string = 'Capitan America';
+
+  arreglo = [ 1 , 2 , 3 , 4, 5 ,6 , 7, 8, 9, 10]
+
+  PI: number = Math.PI;
+
+  porcentaje:number = 0.234
 }
